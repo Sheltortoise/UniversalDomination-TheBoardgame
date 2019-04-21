575ebfa509602c1ce7a0167bd96e1f35da556d9c
Sheltortoise <43179677+Sheltortoise@users.noreply.github.com>
2019-04-20 18:41:52 -0700

    Add files via upload

---
+++ SinglePlayerMenu.java
@@ -0,0 +1,23 @@
+package com.sheltoidusa.universaldominationtheboardgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SinglePlayerMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_player_menu);
+    }
+    public void openCampaignMenu(View view) {
+        Intent intent = new Intent(this, CampaignMenu.class);
+        startActivity(intent);
+    }
+    public void openTestArenaMenu(View view) {
+        Intent intent = new Intent(this, TestArenaMenu.class);
+        startActivity(intent);
+    }
+}
